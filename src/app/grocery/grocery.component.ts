db335dd1d6a2660b53aea556ed82d02a40f9fbe5
rucks15 <rucks15@gmail.com>
2018-09-19 21:41:08 -0700

    strike feature added

--- src/app/grocery/grocery.component.ts
+++ src/app/grocery/grocery.component.ts
@@ -8,14 +8,14 @@ import { Component } from '@angular/core';
 export class GroceryComponent {
   task={
     name:'',
-    id:0
+    id:0,
   };
   tasks = [];
   onClick()
   {
     if(this.task.id == 0)
     {
-      this.tasks.push({id: (new Date()).getTime(), name: this.task.name});
+      this.tasks.push({id: (new Date()).getTime(), name: this.task.name, strike: false});
     }
     this.task={
       name:'',
@@ -33,6 +33,21 @@ export class GroceryComponent {
       if(item.id == this.tasks[i].id)
       {
         this.tasks.splice(i,1);
+        break;
+      }
+    }
+  }
+  onStrike(item){
+    for(var i=0; i<this.tasks.length;i++)
+    {
+      if(item.id == this.tasks[i].id)
+      {
+        if(this.tasks[i].strike){
+          this.tasks[i].strike = false;
+        }
+        else{
+          this.tasks[i].strike = true;
+        }
         break;
       }
     }
